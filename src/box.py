89aa75b7da923584d753e773f1fcb29157a71aa4
NotKieru <isequielnascimento@gmail.com>
2024-09-06 18:42:57 -0300

    update caixa e cano

--- src/box.py
+++ src/box.py
@@ -1,41 +1,34 @@
-import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+import numpy as np
 
-# Parâmetros 
-lado = 10
-altura = 15
-espessura = 1
 
-# Teste de vertices da caixa
-v0 = [0, 0, 0]
-v1 = [lado, 0, 0]
-v2 = [lado, lado, 0]
-v3 = [0, lado, 0]
-v4 = [0, 0, altura]
-v5 = [lado, 0, altura]
-v6 = [lado, lado, altura]
-v7 = [0, lado, altura]
+def create_open_box(side_length):
+    """
+    Cria uma caixa de madeira sem tampa com base quadrada e altura igual ao comprimento dos lados.
+    """
+    # Definição dos vértices da caixa
+    half_side = side_length / 2
+    vertices = np.array([
+        [-half_side, -half_side, 0],  # Vértice 0
+        [half_side, -half_side, 0],   # Vértice 1
+        [half_side, half_side, 0],    # Vértice 2
+        [-half_side, half_side, 0],   # Vértice 3
+        [-half_side, -half_side, side_length],  # Vértice 4
+        [half_side, -half_side, side_length],   # Vértice 5
+        [half_side, half_side, side_length],    # Vértice 6
+        [-half_side, half_side, side_length],   # Vértice 7
+    ])
 
-#  vértices da caixa sem a face superior CORRIGIR depois, a face inferior é que está sendo retirada, pq? não sei, 
-vertices = np.array([v0, v1, v2, v3, v4, v5, v6, v7])
+    # Definição das faces da caixa (sem a tampa) #TODO MUdar pra aramado depois
+    faces = [
+        {'vertices': [0, 1, 2, 3], 'color': 'lightblue'},  # Base
+        {'vertices': [0, 1, 5, 4], 'color': 'lightgrey'},  # Lateral 1
+        {'vertices': [1, 2, 6, 5], 'color': 'lightgrey'},  # Lateral 2
+        {'vertices': [2, 3, 7, 6], 'color': 'lightgrey'},  # Lateral 3
+        {'vertices': [3, 0, 4, 7], 'color': 'lightgrey'},  # Lateral 4
+    ]
 
-# Lista as faces (sem a face superior) 
-faces = [
-    [v0, v1, v5, v4],  # Face inferior
-    [v1, v2, v6, v5],  # Face lateral
-    [v2, v3, v7, v6],  # Face lateral
-    [v3, v0, v4, v7],  # Face lateral
-    [v4, v5, v6, v7]   # Face lateral
-]
-
-# figura e o eixo 3D - TO DO colocar pro grid estar fora da caixa.
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-
-# Adiciona as faces à plotagem
-poly3d = Poly3DCollection(faces, alpha=.25, linewidths=1, edgecolors='r', facecolors='gray')
-ax.add_collection3d(poly3d)
+    return vertices, faces
 
 # Define os limites dos eixos
 ax.set_xlim([0, lado])
